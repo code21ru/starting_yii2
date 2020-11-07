dc946c2602e79598a283da1f1e1a5b2926dd4edf
Code21 <amirvulkan@gmail.com>
2020-11-08 01:21:15 +0300

    Added entry.php view Getting error

---
+++ views/site/entry.php
@@ -0,0 +1,13 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+
+<?php $form = ActiveForm::begin(); //вызываем виджет Формы. Открывающий тег?>
+<?= $form->field($model, 'name'); // input name?>
+<?= $form->field($model, 'emal'); // input email?>
+<div class="form-group">
+    <?= Html::submitButton('Отправить',['class'=>'btn btn-primary']); ?>
+</div>
+<?php $form = ActiveForm::end();?>
+
